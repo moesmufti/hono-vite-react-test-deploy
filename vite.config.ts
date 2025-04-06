3c13f34f53315624ecf176ac0b65b625e1357bc1
moes-91 <m.mufti91@gmail.com>
2025-04-06 15:19:36 +0200

    Enhance project setup by adding Tailwind CSS support and updating scripts for build and preview processes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import reactStack from 'hono-vite-react-stack'
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [reactStack()]
+  plugins: [reactStack(), tailwindcss()]
 })
